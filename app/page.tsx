a0e54836fd725073f1c5b772452ab17276c729a3
Arfatur Rahman <arfatrahman08@gmail.com>
2024-05-07 13:25:07 +0600

    resolve search params issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import FormComponent from "@/component/form-component";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Home | Arfatur Rahman",
+  description: "Practice Bkash payment with sandbox",
+};
 export default function Home() {
   return <FormComponent />;
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,6 +1,10 @@
 import SuccessComponent from "@/component/success-component";
+import { Metadata } from "next";
 import React from "react";
-
+export const metadata: Metadata = {
+  title: "Success | Arfatur Rahman",
+  description: "Practice Bkash payment with sandbox",
+};
 const SuccessPage = () => {
   return <SuccessComponent />;
 };

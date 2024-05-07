fedd985621de3ccf610ff23754c1d1a0ad5e2b5f
Arfatur Rahman <arfatrahman08@gmail.com>
2024-05-07 13:32:52 +0600

    resolve search params issue 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import FormComponent from "@/component/form-component";
 import { Metadata } from "next";
+import { Suspense } from "react";
 export const metadata: Metadata = {
   title: "Home | Arfatur Rahman",
   description: "Practice Bkash payment with sandbox",
 };
 export default function Home() {
-  return <FormComponent />;
+  return (
+    <Suspense>
+      <FormComponent />
+    </Suspense>
+  );
 }

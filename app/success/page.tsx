fadf75713706f866a72197fdb75f879dad424321
Arfatur Rahman <arfatrahman08@gmail.com>
2024-05-07 13:14:34 +0600

    Bkash payment

---
+++ app/success/page.tsx
@@ -0,0 +1,8 @@
+import SuccessComponent from "@/component/success-component";
+import React from "react";
+
+const SuccessPage = () => {
+  return <SuccessComponent />;
+};
+
+export default SuccessPage;
